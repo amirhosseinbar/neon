eba0fa43c243a824765cd118fc427615153f3e9e
amirhosseinbar <iamiriwx@gmail.com>
2024-06-01 12:19:49 +0430

    added change font size feature

--- js/index.js
+++ js/index.js
@@ -1,109 +1,116 @@
-const primaryText = document.getElementById('primary-text');
-const backgroundPreview = document.getElementById('picture');
-const colorsEl = document.querySelectorAll('.color');
-const fontsEl = document.querySelectorAll('.font');
-const backgroundEl = document.querySelectorAll('.tumbnail-background-img');
-const draggable = document.querySelector('.draggable');
-var draggableWidth = backgroundPreview.getBoundingClientRect().width
-var draggableHeight = backgroundPreview.getBoundingClientRect().height
+const primaryText = document.getElementById("primary-text");
+const backgroundPreview = document.getElementById("picture");
+const colorsEl = document.querySelectorAll(".color");
+const fontsEl = document.querySelectorAll(".font");
+const fontSizeEl = document.getElementById("customRange2");
+const backgroundEl = document.querySelectorAll(".tumbnail-background-img");
+const draggable = document.querySelector(".draggable");
+var draggableWidth = backgroundPreview.getBoundingClientRect().width;
+var draggableHeight = backgroundPreview.getBoundingClientRect().height;
 
-
-Array.from(colorsEl).forEach(item=>{
-    item.addEventListener('click',event=>{
-        primaryText.style.textShadow = event.currentTarget.dataset['textshadow']
-    })
-})
-Array.from(fontsEl).forEach(item=>{
-    item.addEventListener('click',event=>{
-        primaryText.style.fontFamily = event.currentTarget.dataset['font'];
-    })
-})
-Array.from(backgroundEl).forEach(item=>{
-    item.addEventListener('click',event=>{
-        backgroundPreview.style.backgroundImage = event.currentTarget.dataset['src'];
-    })
-})
-
+Array.from(colorsEl).forEach((item) => {
+  item.addEventListener("click", (event) => {
+    primaryText.style.textShadow = event.currentTarget.dataset["textshadow"];
+  });
+});
+Array.from(fontsEl).forEach((item) => {
+  item.addEventListener("click", (event) => {
+    primaryText.style.fontFamily = event.currentTarget.dataset["font"];
+  });
+});
+Array.from(backgroundEl).forEach((item) => {
+  item.addEventListener("click", (event) => {
+    backgroundPreview.style.backgroundImage =
+      event.currentTarget.dataset["src"];
+  });
+});
 
 function showFonts() {
-    document.getElementById("fonts-wrapper").classList.toggle('d-none')
+  document.getElementById("fonts-wrapper").classList.toggle("d-none");
 }
 
 function onTextChange() {
-    primaryText.innerText = document.getElementById('input-text').value;
-
+  primaryText.innerText = document.getElementById("input-text").value;
 }
 
-document.getElementById('fileInput').addEventListener('change', function(event) {
+function onFontSizeChange() {
+  const fontSize = fontSizeEl.value + "px";
+  primaryText.style.fontSize = fontSize;
+}
+
+document
+  .getElementById("fileInput")
+  .addEventListener("change", function (event) {
     const file = event.target.files[0];
-    if (file && file.type.startsWith('image/')) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const imageUrl = e.target.result;
-            document.getElementById('picture').style.backgroundImage = `url(${imageUrl})`;
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const imageUrl = e.target.result;
+        document.getElementById(
+          "picture"
+        ).style.backgroundImage = `url(${imageUrl})`;
 
-            // Print the Base64 string in console for verification
-            console.log(imageUrl);
-        };
-        reader.readAsDataURL(file);
+        // Print the Base64 string in console for verification
+        console.log(imageUrl);
+      };
+      reader.readAsDataURL(file);
     } else {
-        alert('لطفاً یک فایل تصویری انتخاب کنید.');
+      alert("لطفاً یک فایل تصویری انتخاب کنید.");
     }
-});
+  });
 
+draggable.addEventListener("mousedown", function (e) {
+  // گرفتن موقعیت اولیه ماوس
+  let shiftX = e.clientX - draggable.offsetLeft;
+  let shiftY = e.clientY - draggable.offsetTop;
 
+  // تابعی برای حرکت دادن المان
+  // function moveAt(pageX, pageY) {
+  //     draggable.style.left = pageX - shiftX + 'px';
+  //     draggable.style.top = pageY - shiftY + 'px';
+  // }
+  function moveAt(pageX, pageY) {
+    let newLeft = pageX - shiftX;
+    let newTop = pageY - shiftY;
 
-
-
-draggable.addEventListener('mousedown', function(e) {
-    // گرفتن موقعیت اولیه ماوس
-    let shiftX = e.clientX - draggable.offsetLeft;
-    let shiftY = e.clientY - draggable.offsetTop;
-
-    // تابعی برای حرکت دادن المان
-    // function moveAt(pageX, pageY) {
-    //     draggable.style.left = pageX - shiftX + 'px';
-    //     draggable.style.top = pageY - shiftY + 'px';
-    // }
-    function moveAt(pageX, pageY) {
-        let newLeft = pageX - shiftX;
-        let newTop = pageY - shiftY;
-
-        // اعمال محدودیت‌های حرکت افقی
-        if (newLeft >= draggableWidth- draggable.getBoundingClientRect().width) {
-            newLeft = draggableWidth - draggable.getBoundingClientRect().width;
-        } else if (newLeft <= 0) {
-            newLeft = 0;
-        }
-
-        if (newTop >= draggableHeight - draggable.getBoundingClientRect().height - 200) {
-            newTop = draggableHeight - draggable.getBoundingClientRect().height - 200;
-        } else if (newTop < 0) {
-            newTop = 0;
-        }
-
-        draggable.style.left = newLeft + 'px';
-        draggable.style.top = newTop + 'px';
+    // اعمال محدودیت‌های حرکت افقی
+    if (newLeft >= draggableWidth - draggable.getBoundingClientRect().width) {
+      newLeft = draggableWidth - draggable.getBoundingClientRect().width;
+    } else if (newLeft <= 0) {
+      newLeft = 0;
     }
 
-    // حرکت دادن المان به موقعیت اولیه ماوس
-    moveAt(e.pageX, e.pageY);
-
-    // تابعی برای جابجایی المان با حرکت ماوس
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
+    if (
+      newTop >=
+      draggableHeight - draggable.getBoundingClientRect().height - 200
+    ) {
+      newTop = draggableHeight - draggable.getBoundingClientRect().height - 200;
+    } else if (newTop < 0) {
+      newTop = 0;
     }
 
-    // گوش دادن به رویداد حرکت ماوس
-    document.addEventListener('mousemove', onMouseMove);
+    draggable.style.left = newLeft + "px";
+    draggable.style.top = newTop + "px";
+  }
 
-    // حذف گوش دادن به رویداد حرکت ماوس زمانی که ماوس را رها می‌کنیم
-    document.addEventListener('mouseup', function() {
-        document.removeEventListener('mousemove', onMouseMove);
-    });
+  // حرکت دادن المان به موقعیت اولیه ماوس
+  moveAt(e.pageX, e.pageY);
 
-    // جلوگیری از کشیدن متن در مرورگر
-    draggable.ondragstart = function() {
-        return false;
-    };
+  // تابعی برای جابجایی المان با حرکت ماوس
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // گوش دادن به رویداد حرکت ماوس
+  document.addEventListener("mousemove", onMouseMove);
+
+  // حذف گوش دادن به رویداد حرکت ماوس زمانی که ماوس را رها می‌کنیم
+  document.addEventListener("mouseup", function () {
+    document.removeEventListener("mousemove", onMouseMove);
+  });
+
+  // جلوگیری از کشیدن متن در مرورگر
+  draggable.ondragstart = function () {
+    return false;
+  };
 });
